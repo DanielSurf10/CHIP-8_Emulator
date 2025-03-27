0ba91fa56c8629b635160c22d39e78372f7ae051
DanielSurf10 <daniel101surf@gmail.com>
2025-03-27 16:31:26 -0300

    FEATURE: Adicionada função update_timers para gerenciar temporizadores do chip8

--- src/main.c
+++ src/main.c
@@ -103,6 +103,13 @@ void	set_display_pixel(uint8_t *display, int x, int y, int state) {
 	display[WIDTH * y + x] = state;
 }
 
+void	update_timers(chip8 *chip8_data) {
+	if (chip8_data->delay_timer > 1)
+		chip8_data->delay_timer--;
+	if (chip8_data->sound_timer > 1)
+		chip8_data->sound_timer--;
+}
+
 int main(int argc, char *argv[]) {
 
 	SDL_Window		*window;
@@ -132,6 +139,8 @@ int main(int argc, char *argv[]) {
 			}
 		}
 
+		update_timers(chip8_data);
+
 		for (int i = 0; i < 5; i++)
 		{
 			set_display_pixel(chip8_data->display, 0 + 30, i + 12, chip8_data->memory[0x50 + i + num * 5] & 0b10000000);
@@ -139,6 +148,7 @@ int main(int argc, char *argv[]) {
 			set_display_pixel(chip8_data->display, 2 + 30, i + 12, chip8_data->memory[0x50 + i + num * 5] & 0b00100000);
 			set_display_pixel(chip8_data->display, 3 + 30, i + 12, chip8_data->memory[0x50 + i + num * 5] & 0b00010000);
 		}
+
 
 		if (num == 15)
 			num = 0;
