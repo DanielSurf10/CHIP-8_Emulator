955c5aaf0e23520a41971a3e3af9bb4ae1716939
DanielSurf10 <daniel101surf@gmail.com>
2025-03-26 23:44:39 -0300

    FEATURE: Adicionada fonte na memória do chip8

--- src/main.c
+++ src/main.c
@@ -1,8 +1,38 @@
 #include "chip8.h"
 
+void	init_font(chip8 *chip_data)
+{
+	// There are 16 characters (sprites) with 4 pixels width
+	// and 5 pixels height
+	const uint8_t	font_size = 16 * 5;
+	const uint8_t	font_start_address = 0x50;
+	const uint8_t	font[] = {
+		0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+		0x20, 0x60, 0x20, 0x20, 0x70, // 1
+		0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+		0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+		0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+		0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+		0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+		0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+		0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+		0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+		0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+		0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+		0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+		0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+		0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+		0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+	};
+
+	memcpy(&chip_data->memory[font_start_address], font, font_size);
+}
+
 void	chip8_init(chip8 **chip8_data) {
 	*chip8_data = malloc(sizeof(chip8));
 	bzero(*chip8_data, sizeof(chip8));
+
+	init_font(*chip8_data);
 }
 
 void	draw_chip8_display(SDL_Renderer *renderer, chip8 *chip8_data) {
@@ -75,9 +105,10 @@ int main(int argc, char *argv[]) {
 		return (-1);
 
 	chip8_init(&chip8_data);
-	chip8_data->display[0] = 1;
+	// chip8_data->display[0] = 1;
 
-	int line = 0, column = 0;
+	// int line = 0, column = 0;
+	int	num = 0;
 
 	// Loop principal
 	running = 1;
@@ -91,21 +122,40 @@ int main(int argc, char *argv[]) {
 
 		// Atualize o estado do CHIP-8 aqui
 
-		if (HEIGHT <= line)
-			line = 0;
-		else if (WIDTH > column)
-			column++;
-		else
+// 		if (HEIGHT <= line)
+// 			line = 0;
+// 		else if (WIDTH > column)
+// 			column++;
+// 		else
+// 		{
+// 			line++;
+// 			column = 0;
+// 		}
+//
+// 		memset(chip8_data->display, 0, HEIGHT * WIDTH * sizeof(uint8_t));
+// 		chip8_data->display[column + line * WIDTH] = 1;
+
+// 		memcpy(chip8_data->display + WIDTH * 0, chip8_data->memory + 0x50 + num * 5, 5);
+// 		memcpy(chip8_data->display + WIDTH * 1, chip8_data->memory + 0x50 + num * 5 + 5, 5);
+// 		memcpy(chip8_data->display + WIDTH * 2, chip8_data->memory + 0x50 + num * 5 + 10, 5);
+// 		memcpy(chip8_data->display + WIDTH * 3, chip8_data->memory + 0x50 + num * 5 + 11, 5);
+// 		memcpy(chip8_data->display + WIDTH * 4, chip8_data->memory + 0x50 + num * 5 + 12, 5);
+
+		for (int i = 0; i < 5; i++)
 		{
-			line++;
-			column = 0;
+			chip8_data->display[(i + 12) * WIDTH + 30 + 0] = chip8_data->memory[0x50 + i + num * 5] & 0b10000000;
+			chip8_data->display[(i + 12) * WIDTH + 30 + 1] = chip8_data->memory[0x50 + i + num * 5] & 0b01000000;
+			chip8_data->display[(i + 12) * WIDTH + 30 + 2] = chip8_data->memory[0x50 + i + num * 5] & 0b00100000;
+			chip8_data->display[(i + 12) * WIDTH + 30 + 3] = chip8_data->memory[0x50 + i + num * 5] & 0b00010000;
 		}
 
-		memset(chip8_data->display, 0, HEIGHT * WIDTH * sizeof(uint8_t));
-		chip8_data->display[column + line * WIDTH] = 1;
+		if (num == 15)
+			num = 0;
+		else
+			num++;
 
 		draw_chip8_display(renderer, chip8_data);
-		SDL_Delay(1000 / 60);
+		SDL_Delay(1000 / 2);
 	}
 
 	free(chip8_data);
